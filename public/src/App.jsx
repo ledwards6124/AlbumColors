3dd5203fa6fe70b497b48512330192803e078938
Luke <lce1868@g.rit.edu>
2025-04-02 10:02:50 -0400

    initial commit

---
+++ public/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Vite + React</h1>
+      <p>
+        <a
+          href="https://vitejs.dev/guide/features.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn more
+        </a>
+        about Vite.
+      </p>
+    </div>
+  )
+}
