8622f6df3b621c08ba6176856d1ce849c1df6284
Luke <lce1868@g.rit.edu>
2025-04-07 11:40:28 -0400

    fixing routing issue when deploying to pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './css/index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/AlbumColors'>
     <App />
   </BrowserRouter>,
 )
