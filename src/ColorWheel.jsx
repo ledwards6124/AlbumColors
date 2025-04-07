cd1bc91f650ca859d7a4e6b54579b03b6bc024be
Luke <lce1868@g.rit.edu>
2025-04-07 11:35:58 -0400

    completed navbar routing and css

--- src/ColorWheel.jsx
+++ src/ColorWheel.jsx
@@ -1,52 +1,40 @@
 import React from 'react'
 import './css/ColorWheel.css'
+
 import red from './assets/colorwheel/Solid_red.svg'
 import orange from './assets/colorwheel/Solid_orange.svg'
 import yellow from './assets/colorwheel/Solid_yellow.svg'
 import green from './assets/colorwheel/Solid_green.svg'
 import blue from './assets/colorwheel/Solid_blue.svg'
 import purple from './assets/colorwheel/Solid_purple.svg'
-import white from './assets/colorwheel/Solid_white.svg'
 import black from './assets/colorwheel/Solid_black.svg'
+import white from './assets/colorwheel/Solid_white.svg'
 
-const colorSvgs = [
-  { src: orange, route: '/orange' },
-  { src: yellow, route: '/yellow' },
-  { src: green, route: '/green' },
-  { src: blue, route: '/blue' },
-  { src: purple, route: '/purple' },
-  { src: white, route: '/white' },
-  { src: black, route: '/black' },
-  { src: red, route: '/red' },
-  
+export default function ColorWheel({navigate}) {
 
-]
-
-
-export default function ColorWheel({ navigate }) {
-  return (
-    <div className="image-color-wheel">
-{[...colorSvgs].map((slice, i, arr) => {
-    const angle = ((arr.length - 1 - i) * 45) % 360
-
+  const colors = [red, orange, yellow, green, blue, purple, black, white]
+  const links = ['/red', '/orange', '/yellow', '/green', '/blue', '/purple', '/black', '/white']
+  const radius = 180 // distance from center
 
   return (
-    <div
-      key={i}
-      className="slice-wrapper"
-      style={{
-        transform: `rotate(${angle}deg)`,
-      }}
-      onClick={() => navigate(slice.route)}
-    >
-      <img src={slice.src} alt={slice.route} className="slice-image" />
-    </div>
-  )
-})}
-
-
-
-
+    <div className="wheel-rotator">
+      <div className="dot-wheel">
+        {colors.map((color, i) => {
+          const angle = (360 / colors.length) * i
+          return (
+            <img
+              src={color}
+              alt={color}
+              key={i}
+              className="dot"
+              style={{
+                 transform: `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg)`,
+              }}
+              onClick={() => navigate(`${links[i]}`)}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom'
+import './css/Navbar.css'
 
 export default function Navbar() {
-    return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/red">Red Album</Link>
-            <Link to="/orange">Orange Album</Link>
-            <Link to="/yellow">Yellow Album</Link>
-            <Link to="/green">Green Album</Link>
-            <Link to="/blue">Blue Album</Link>
-            <Link to="/purple">Purple Album</Link>
-            <Link to="/black">Black Album</Link>
-            <Link to="/white">White Album</Link>
-        </div>
-    )
+  return (
+    <nav>
+      <Link className="nav-link" to="/">Home</Link>
+      <Link className="nav-link red" to="/red">Red Album</Link>
+      <Link className='nav-link orange' to="/orange">Orange Album</Link>
+      <Link className='nav-link yellow' id='yellow' to="/yellow">Yellow Album</Link>
+      <Link className='nav-link green' id='green' to="/green">Green Album</Link>
+      <Link className='nav-link blue' id='blue' to="/blue">Blue Album</Link>
+      <Link className='nav-link purple' id='purple' to="/purple">Purple Album</Link>
+      <Link className='nav-link black' id='black' to="/black">Black Album</Link>
+      <Link className='nav-link white' id='white' to="/white">White Album</Link>
+    </nav>
+  )
 }
